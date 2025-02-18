78668497f380400b7d2d6ad511988b065bb63d5d
easkalm <almas.askarov@ericsson.com>
2025-02-18 01:03:27 +0100

    Added keywords POST route

--- main.py
+++ main.py
@@ -16,7 +16,7 @@ def fetch_wikipedia_page(article):
         return None
 
 def clean_text(text):
-    words = re.findall(r"\b[\w'-]+\b", text.lower())  # Match words of any length
+    words = re.findall(r"\b[\w'-]+\b", text.lower())
     return words
 
 def calculate_word_frequencies(words):
@@ -36,17 +36,40 @@ def extract_links(content_soup):
 
 @app.route("/word-frequency", methods=["GET"])
 def word_frequency():
-    if "article" not in request.args or "depth" not in request.args:
-        return jsonify({"error": "Article and depth parameters are required"}), 400
+    if not all(param in request.args for param in ["article", "depth"]):
+        return jsonify({"error": "article, depth parameters are required"}), 400
     article = request.args.get("article")
     depth = int(request.args.get("depth"))
+
     content_soup = fetch_wikipedia_page(article)
     if not content_soup: return jsonify({"error": "Article not found"}), 404
     text = content_soup.get_text()
     words = clean_text(text)
     word_freq = calculate_word_frequencies(words)
     links = extract_links(content_soup)
+    print(links)
     return jsonify(word_freq)
+
+@app.route("/keywords", methods=["POST"])
+def keywords():
+    data = request.get_json()
+    if not all(param in data for param in ["article", "depth", "ignore_list", "percentile"]):
+        return jsonify({"error": "article, depth, ignore_list, percentile parameters are required"}), 400
+    article = data.get("article")
+    depth = int(data.get("depth", 1))
+    ignore_list = set(data.get("ignore_list"))
+    percentile = int(data.get("percentile"))
+
+    content_soup = fetch_wikipedia_page(article)
+    if not content_soup: return jsonify({"error": "Article not found"}), 404
+    text = content_soup.get_text()
+    words = clean_text(text)
+    word_freq = calculate_word_frequencies(words)
+    links = extract_links(content_soup)
+    print(links)
+    filtered_freq = {word: freq for word, freq in word_freq.items()
+                     if word not in ignore_list and freq["percentage"] >= percentile}
+    return jsonify(filtered_freq)
 
 if __name__ == "__main__":
     app.run(debug=True)
